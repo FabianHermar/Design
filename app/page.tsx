589a006c9393530cf8f08c2bc2e47e48b1383922
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-06-13 00:02:55 -0600

    🔌 Home component connected

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header'
+import HomeD from '@/components/HomeD'
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <HomeD />
     </main>
   )
 }
